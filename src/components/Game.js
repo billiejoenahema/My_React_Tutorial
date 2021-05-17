59b92810f253628ca087319b24da9239f031d914
billiejoenahema <billiejoenahema@gmail.com>
2021-05-17 20:39:21 +0900

    refactoring: separate function from Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Board from './Board'
 import Moves from './Moves'
+import { calculateWinner } from '../calculateWinner'
 
 const Game = React.memo(() => {
   const [history, setHistory] = useState([{
@@ -41,7 +42,7 @@ const Game = React.memo(() => {
   if (winner) {
     status = `勝者: ${winner}`
   } else {
-    status = `次のプレーヤー: ${xIsNext ? "X" : "O"}`
+    status = `次のプレーヤー: ${xIsNext ? 'X' : 'O'}`
   }
 
   return (
@@ -57,23 +58,3 @@ const Game = React.memo(() => {
   )
 })
 export default Game
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null
-}
